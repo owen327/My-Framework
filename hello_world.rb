d01b910eb03626e50f4fb9b2bf6da2025bc56887
Owen Tran <owen_327@hotmail.com>
2018-04-04 13:14:58 +0100

    Add advice.rb

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,6 @@
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World']]
+    ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    #  ['200', {'Content-Type' => 'text/plain'}, [env.inspect.to_s]]
   end
 end
